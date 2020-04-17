91c58aae3d6f13459b15c48194b22f8a1671077a
Andre <ajparker000@gmail.com>
2020-04-16 17:51:14 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+  console.log('listening now on port 3000');
+});
