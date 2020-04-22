a2beaa9bcc6160450fa485e5d298b07f166f3e4a
Andre <ajparker000@gmail.com>
2020-04-22 16:52:39 -0700

    updated server

--- server.js
+++ server.js
@@ -25,16 +25,22 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
+
   db.select('email', 'hash')
     .from('login')
-    .where('email', '=', req.body.email)
+    .where('email', '=', email)
     .then((data) => {
-      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      const isValid = bcrypt.compareSync(password, data[0].hash);
       if (isValid) {
         return db
           .select('*')
           .from('users')
-          .where('email', '=', req.body.email)
+          .where('email', '=', email)
           .then((user) => {
             res.json(user[0]);
           })
@@ -48,6 +54,10 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
   const { email, password, name } = req.body;
+
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
 
   const hash = bcrypt.hashSync(password);
 
