54ae501b927456c461e5e29b49bc1392f8573d71
Andre <ajparker000@gmail.com>
2020-04-23 11:28:51 -0700

    adding database connection

--- server.js
+++ server.js
@@ -7,10 +7,8 @@ const knex = require('knex');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-graceful-82433',
-    user: 'postgres',
-    password: 'test123',
-    database: 'facerecognitionapp',
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
